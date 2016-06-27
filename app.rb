d75f0b9ea849d6d39531019f7bbb0924d56ecf1e
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-27 11:48:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  # route for directory index
 	get '/' do
-		
+	 erb :index
 	end
+
+  # route for info
+  get '/info' do
+    erb :info
+  end
 end
